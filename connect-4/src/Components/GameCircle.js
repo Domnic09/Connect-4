c64adccd9114c5c4b871da925b9d0408f3bbfb72
Domnic09 <dominictaboada74@gmail.com>
2024-01-29 18:34:32 +0800

    Update GameCircle.js

--- connect-4/src/Components/GameCircle.js
+++ connect-4/src/Components/GameCircle.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
-const onClick = () =>{
-    alert('on click')
+const onClick = (ev, id, value) =>{
+  debugger;
+  alert('on click' + id);
 }
 
-const GameCircle = () => {
+const GameCircle = ({ id, value, children }) => {
+  console.log(id);
   return (
-    <div onClick={onClick}>
-        GameCircle
+    <div onClick={(ev) => onClick(ev, id, value)}>
+      {children}
     </div>
-  );
-};
+  )
+}
 
 export default GameCircle;
