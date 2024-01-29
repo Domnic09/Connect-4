db85539914989e725093ab708c32fc508c0c606c
Domnic09 <dominictaboada74@gmail.com>
2024-01-29 18:34:53 +0800

    Update GameBoard.js

--- connect-4/src/Components/GameBoard.js
+++ connect-4/src/Components/GameBoard.js
@@ -1,16 +1,39 @@
-import React from "react";
+import React from 'react';
+import GameCircle from './GameCircle';
 
-import GameCircle from "./GameCircle";
+const Gameboard = () => {
+//   const handleClick = () => {
+//     alert('on click');
+//   };
 
-const Gameboard = () =>{
-    return  (
+  return (
     <div>
-        <GameCircle />
-        <GameCircle />
-        <GameCircle />
-        <GameCircle />
+      <GameCircle id={1} value={5} >
+        Red
+      </GameCircle>
+      <GameCircle id={2}  >
+        Blue
+      </GameCircle>
+      <GameCircle id={3}  >
+        Red
+        </GameCircle>
+      <GameCircle id={4}  >
+        Blue
+      </GameCircle>
+      <GameCircle id={5}  >
+        Red
+        </GameCircle>
+      <GameCircle id={6} >
+        Blue
+        </GameCircle>
+      <GameCircle id={7}  >
+        Red
+        </GameCircle>
+      <GameCircle id={8}  >
+        Blue
+        </GameCircle>
     </div>
-    )
-}
+  );
+};
 
 export default Gameboard;
